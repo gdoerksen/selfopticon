f3937f09edf0534a38ba8b3cf8c66ee30c3f233d
Graham Doerksen <46031209+gdoerksen@users.noreply.github.com>
2025-07-27 12:25:46 -0600

    Refactor to uv monorepo, spotify-watcher is still an application

--- projects/spotify-watcher/__init__.py
+++ projects/spotify-watcher/__init__.py
@@ -1,10 +1,6 @@
 """
 Spotify Watcher - A Python application to track Spotify listening history
 """
-
-__version__ = "1.0.0"
-__author__ = "Your Name"
-
 from .auth import RequestUserAuthorization, RequestAccessToken, RefreshToken
 from .spotify_api import GetRecentlyPlayed
 
